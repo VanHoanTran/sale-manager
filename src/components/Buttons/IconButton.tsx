5f460c7c58124db0b1b4a471f760532b33566833
vanhoantran <vanhoantranca@gmail.com>
2023-10-20 11:43:40 -0400

    create IconButton

---
+++ src/components/Buttons/IconButton.tsx
@@ -0,0 +1,15 @@
+import { IconButtonProps } from '../../constants/Props';
+
+const IconButton = ({ onClick, icon }: IconButtonProps) => {
+  console.log(icon);
+  return (
+    <button
+      className=" hover:bg-slate-100 rounded-full p-2 mx-1 transition-all ease-in duration-400 "
+      onClick={onClick}
+    >
+      <span className="avatar-span">{icon}</span>
+    </button>
+  );
+};
+
+export default IconButton;

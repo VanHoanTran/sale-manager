bbfe792e7d7b9c9b3857873303bcc435110fbcff
vanhoantran <vanhoantranca@gmail.com>
2023-12-13 11:40:47 -0500

    created an order list component

--- src/components/OrderList/index.tsx
+++ src/components/OrderList/index.tsx
@@ -1,6 +1,6 @@
 import { SortProvider } from '../../contexts/SortContext';
 import { Table } from '../common/Table';
-import TableRow from '../common/Table/TableBodyRow';
+import TableBodyRow from '../common/Table/TableBodyRow';
 import Title from '../common/Title';
 import { Transaction } from '../../constants/Props';
 import { transactions } from '../../data/transactions';
@@ -11,6 +11,13 @@ import OrderStatus from './OrderStatus';
 import OrderTotal from './OrderTotal';
 import OrderPayment from './OrderPayment';
 import Pagination from '../common/Pagination';
+import Container from '../common/Containers/ListContainer';
+import ActionGroup from '../common/Actions/ActionGroup';
+import Toggle from '../common/Actions/Toggle';
+import ActionList from '../common/Actions/ActionList';
+import ActionItem from '../common/Actions/ActionItem';
+import { PencilIcon, TrashIcon } from '@heroicons/react/24/outline';
+import ActionsProvider from '../../contexts/ActionsProvider';
 const titles = [
   { title: 'order', keySort: 'order' },
   { title: 'date', keySort: 'date' },
@@ -22,32 +29,53 @@ const titles = [
 ];
 
 const OrderList = () => {
+  const handleDelete = () => {
+    console.log('Delete');
+  };
+
+  const handleEdit = () => {
+    console.log('Edit');
+  };
   return (
-    <SortProvider>
-      <Title>Products</Title>
-      <Table data={transactions as []} titles={titles as []}>
-        <Table.Header />
-        <Table.Body
-          render={(trans: Transaction) => (
-            <TableRow>
-              <OrderNumber id={trans.id} />
-              <OrderDate date={trans.date} />
-              <OrderCustomer customer={trans.customer} />
-              <OrderStatus status={trans.status} />
-              <OrderPayment
-                paymentIdentifier={trans.paymentIdentifier}
-                paymentMethod={trans.paymentMethod}
-              />
-              <OrderTotal total={trans.totalAmount} />
-              <td>...</td>
-            </TableRow>
-          )}
-        />
-        <Table.Footer>
-          <Pagination />
-        </Table.Footer>
-      </Table>
-    </SortProvider>
+    <Container>
+      <SortProvider>
+        <Title>Order List</Title>
+        <ActionsProvider>
+          <Table data={transactions as []} titles={titles as []}>
+            <Table.Header />
+            <Table.Body
+              render={(trans: Transaction) => (
+                <TableBodyRow>
+                  <OrderNumber id={trans.id} />
+                  <OrderDate date={trans.date} />
+                  <OrderCustomer customer={trans.customer} />
+                  <OrderStatus status={trans.status} />
+                  <OrderPayment
+                    paymentIdentifier={trans.paymentIdentifier}
+                    paymentMethod={trans.paymentMethod}
+                  />
+                  <OrderTotal total={trans.totalAmount} />
+                  <ActionGroup>
+                    <Toggle id={trans.id.toString()} />
+                    <ActionList id={trans.id.toString()}>
+                      <ActionItem icon={<TrashIcon />} onClick={handleDelete}>
+                        Delete
+                      </ActionItem>
+                      <ActionItem icon={<PencilIcon />} onClick={handleEdit}>
+                        Edit
+                      </ActionItem>
+                    </ActionList>
+                  </ActionGroup>
+                </TableBodyRow>
+              )}
+            />
+            <Table.Footer>
+              <Pagination />
+            </Table.Footer>
+          </Table>
+        </ActionsProvider>
+      </SortProvider>
+    </Container>
   );
 };
 
